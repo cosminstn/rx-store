c3e0d685eff56ae15b975c1dadb269a0f1a56951
Cosmin Stoian <cosmyn9708@gmail.com>
2020-12-16 11:45:52 +0200

    Added some docs to Store.java

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -2,6 +2,12 @@ import com.jakewharton.rxrelay3.BehaviorRelay;
 import com.jakewharton.rxrelay3.Relay;
 import io.reactivex.rxjava3.core.Observable;
 
+/**
+ * Inspired from <a href="https://gist.githubusercontent.com/czyrux/f46d818d65cbeee4e80de7ba4504252e/raw/2920355cc8b70d5261c4949a9565e3f979a7475b/Store.java">Store.java</a>
+ * and adapted for rxjava3 and rxrelay3.
+ * @author Cosmin Stoian
+ * @param <T>
+ */
 public class Store<T> {
 	private final Relay<T> storeSubject;
 
